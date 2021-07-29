e89503685cec7c8670b0f718be6727c17457b9ad
eli.sutton <elisutton0721@gmail.com>
2021-07-29 15:55:20 -0400

    Made readability and PEP8 compliance improvements

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,4 +1,7 @@
 def convert(number):
     potential_factors = ((3, 'Pling'), (5, 'Plang'), (7, 'Plong'))
-    sounds = ''.join( pf[1] for pf in potential_factors if number % pf[0] == 0 )
+    sounds = ''.join(
+        sound
+        for factor, sound in potential_factors 
+        if number % factor == 0)
     return sounds or str(number)
