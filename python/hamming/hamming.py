d0a680906ab378c241bb282c355b5d55b7b55e4d
eli.sutton <elisutton0721@gmail.com>
2021-08-03 11:40:45 -0400

    Simplified and made solution more Pythonic

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,10 +1,4 @@
 def distance(strand_a, strand_b):
-    if len(strand_a) == len(strand_b):
-        diffs = 0
-        strands_zipped = [*zip(strand_a, strand_b)]
-        for char1, char2 in strands_zipped:
-            if char1 != char2:
-                diffs += 1
-        return diffs
-    else:
+    if len(strand_a) != len(strand_b):
         raise ValueError('The strands are of different lengths, so the Hamming distance cannot be computed.')
+    return sum([1 for char1, char2 in zip(strand_a, strand_b) if char1 != char2])
