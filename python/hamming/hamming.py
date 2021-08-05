e4f7fe7c54cfcc9ff36607b3d15df2b25fbfd852
eli.sutton <elisutton0721@gmail.com>
2021-08-05 12:21:24 -0400

    Improved list comprehension in return statement and converted to generator comprehension

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('The strands are of different lengths, so the Hamming distance cannot be computed.')
-    return sum([1 for char1, char2 in zip(strand_a, strand_b) if char1 != char2])
+    return sum(char1 != char2 for char1, char2 in zip(strand_a, strand_b))
